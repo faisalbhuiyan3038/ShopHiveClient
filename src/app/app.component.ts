b2ec83fbd1c6c008f64456e1fab9701e1a2ec5fb
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2023-07-19 19:06:41 +0600

    request category data from frontend successfully

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ShopHiveClient';
+  categories: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:7243/api/Category").subscribe({
+      next: response => this.categories = response,
+      error: error => console.log(error),
+      complete: () => console.log("Request has completed.")
+    });
+  }
 }
