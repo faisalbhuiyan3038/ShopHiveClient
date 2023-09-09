887d0dc1c9cad870344e367346a815ab52679701
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2023-09-09 12:38:29 -0700

    added breadcrumbs to all pages

--- src/app/shop/product-details/product-details.component.ts
+++ src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IProduct } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -11,7 +12,7 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductDetailsComponent implements OnInit {
   product: IProduct | null=null;
 
-  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute){}
+  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute, private bcService: BreadcrumbService){}
 
   ngOnInit(){
     this.loadProduct();
@@ -22,7 +23,7 @@ export class ProductDetailsComponent implements OnInit {
     const productId = productIdString ? parseInt(productIdString, 10) : null;
     if(productId!=null){
       this.shopService.getProductById(productId).subscribe({
-        next: (response) => {this.product = response},
+        next: (response) => {this.product = response; this.bcService.set('@productDetails',this.product.name)},
         error: error => console.log(error),
         complete: () => console.log("Request has completed.")
       });
