8a9e2131aa2605b603a2d779109a1495c039d771
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2023-09-08 19:54:38 -0700

    add test component for errors

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://your-production-api.com', // Your production API URL
+  };
+  
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:7243/api/', // Your development API URL
+  };
+  
