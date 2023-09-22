0c881868a4e1ceb1ea0ebcfd9be187d3ff7212ff
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2023-09-22 12:27:57 -0700

    add increment,decrement function, create order summary, routing for checkout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
-import { CarouselModule } from 'ngx-bootstrap/carousel'
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { OrderTotalsComponent } from './components/order-totals/order-totals.component'
 
 
 @NgModule({
   declarations: [
    
+  
+    OrderTotalsComponent
   ],
   imports: [
     CommonModule,
@@ -14,6 +17,7 @@ import { CarouselModule } from 'ngx-bootstrap/carousel'
   ],
   exports: [
     CarouselModule,
+    OrderTotalsComponent
   ]
 })
 export class SharedModule { }
