98012ed9885e51d4df02af095e055588310f78c8
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2023-10-01 08:14:41 -0700

    added login validation and fixed register form

--- src/app/account/register/register.component.ts
+++ src/app/account/register/register.component.ts
@@ -3,6 +3,7 @@ import { IRegisterUser } from 'src/app/shared/models/user';
 import { AccountService } from '../account.service';
 import { Router } from '@angular/router';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { customValidationMessages } from './validationMessages';
 
 @Component({
   selector: 'app-register',
@@ -21,29 +22,49 @@ export class RegisterComponent implements OnInit {
     address: '',
   };
 
+  customValidationMessages = customValidationMessages;
+
   constructor(private accountService: AccountService, private router: Router) {}
   ngOnInit(): void {
     this.createRegisterForm();
   }
-
-  createRegisterForm(){
-    this.registerForm = new FormGroup({
-      username: new FormControl('',Validators.required),
-      firstName: new FormControl('', Validators.required),
-      lastName: new FormControl('', Validators.required),
-      email: new FormControl('', Validators.required),
-      mobile: new FormControl('', Validators.required),
-      address: new FormControl('', Validators.required),
-      password: new FormControl('', Validators.required)
-    })
+  
+  passwordValidator(control: FormControl) {
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,13}$/;
+    const valid = passwordPattern.test(control.value);
+  
+    return valid ? null : { passwordPattern: true };
   }
+  
+
+  createRegisterForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      mobile: new FormControl('', [Validators.required, Validators.pattern(/^\d{11}$/)]),
+      address: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
+  
+    // Set custom error messages for each field
+    this.registerForm.get('username').setErrors(customValidationMessages);
+    this.registerForm.get('firstName').setErrors(customValidationMessages);
+    this.registerForm.get('lastName').setErrors(customValidationMessages);
+    this.registerForm.get('email').setErrors(customValidationMessages);
+    this.registerForm.get('mobile').setErrors(customValidationMessages);
+    this.registerForm.get('address').setErrors(customValidationMessages);
+    this.registerForm.get('password').setErrors(customValidationMessages);
+  }
+  
 
   onSubmit() {
     console.log(this.registerForm.value);
     this.accountService.registerUser(this.registerForm.value).subscribe(
       (response) => {
         // Handle successful registration here
-        this.router.navigateByUrl('/Account/login'); // Redirect to the login page
+        this.router.navigateByUrl('/account/login'); // Redirect to the login page
       },
       (error) => {
         // Handle registration error here
---
+++ src/app/account/register/validationMessages.ts
@@ -0,0 +1,9 @@
+// validation-messages.ts
+export const customValidationMessages = {
+    required: 'This field is required.',
+    email: 'Please enter a valid email address.',
+    pattern: 'Please enter a valid 11-digit mobile number.',
+    minlength: 'Password must be at least 8 characters long.',
+    passwordPattern: 'Password requires one lowercase letter, one uppercase letter, one digit, 6-13 length.',
+  };
+  
